3ae0a0417cbb562b24f9992cce28c6d149df5b07
Not-Bean <alexander.bean@ontariotechu.net>
2026-09-20 10:48:19 -0400

    Enemy Object
    added enemies that follow a spline, towers look at the closest enemy

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Splines;
+using Unity.Mathematics;
+
+public class EnemyScript : MonoBehaviour
+{
+    /*
+     TODO: 
+     allow inheritance 
+     allow instantiation
+     player takes damage when the enemy reaches the end of the spline
+    */
+    
+    public int health = 100;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+}
--- Assets/Scripts/TowerTest.cs
+++ Assets/Scripts/TowerTest.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class TowerTest : TowerScript
@@ -14,4 +15,28 @@ public class TowerTest : TowerScript
         Debug.Log("Override Example");
     }
 
+    public GameObject FindClosestEnemy()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+
+    void Update()
+    {
+        transform.LookAt(FindClosestEnemy().transform);
+    }
 }
