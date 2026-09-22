26ff68a395eee77ceeba14350f9949a01088ec8d
Shallowcat <joshR364@hotmail.com>
2026-09-22 12:08:18 -0400

    Clean Slate

--- Assets/Towers/TowerData/TowerData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName ="TowerData", menuName = "ScriptableObjects/TowerData")]
-public class TowerData : ScriptableObject
-{
-    public string TowerName;
-    public string TowerDescription;
-}
